d8f7ba6aa8526d494a03b3c7bfcad4bad0299e4d
Sanjay Kumar <sanjaynabha7@gmail.com>
2024-05-03 12:54:57 +0530

    Upload Project ReactGithubFinder-master

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Segment, Header } from "semantic-ui-react";
+
+function About() {
+  return (
+    <Segment>
+      <Header as="h4" content="Github Finder React App" />
+      <p>Version: 1.0.0</p>
+      <p>Developed By: Sanjay Kumar</p>
+    </Segment>
+  );
+}
+export default About;
